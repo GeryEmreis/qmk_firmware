933b0db88901c9beaa710fa4f8fa76607ad66fb4
Pavel Geiger <geiger@jablotron.cz>
2019-10-31 12:56:59 +0100

    Disable some rules

--- keyboards/preonic/rev3/rules.mk
+++ keyboards/preonic/rev3/rules.mk
@@ -9,8 +9,8 @@ BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration(+1000)
 ## (Note that for BOOTMAGIC on Teensy LC you have to use a custom .ld script.)
 MOUSEKEY_ENABLE = yes      # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
-CONSOLE_ENABLE = yes         # Console for debug(+400)
-COMMAND_ENABLE = yes       # Commands for debug and configuration
+CONSOLE_ENABLE = no         # Console for debug(+400)
+COMMAND_ENABLE = no       # Commands for debug and configuration
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
